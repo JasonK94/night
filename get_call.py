8884ea913b044fb5821d0dccbcbaf2661106f10b
dasdasda <eaoaeaoa@gmail.com>
2022-10-22 18:28:24 +0900

    221022

--- get_call.py
+++ get_call.py
@@ -71,33 +71,8 @@ while True and not kb.is_pressed('Esc'):
             xy = pa.position()
             absents_new_=pa.locateAllOnScreen('absent.png', confidence=cf)
             absents_new=[absent for absent in absents_new_]
-            if not pa.locateOnScreen('call.png', confidence=cf):
-                errors_call+=1
-                if errors_call>5:
-                    call = client.calls.create(
-                        url='http://demo.twilio.com/docs/voice.xml',
-                        to='+821083782358',
-                        from_='+19204770393'
-                    )
-                else:
-                    print('전화 기록 최상단으로 이동합니다')
-                    pa.moveTo(XY_0[0] + 200, XY_0[1] + 500)
-                    pa.scroll(5000)
-            elif pa.locateOnScreen('call_absent.png'):
-                message = client.messages \
-                    .create(body='부재중 전화가 늘어났다', from_='+19204770393', to='+821083782358')
-                call = client.calls.create(
-                    url='http://demo.twilio.com/docs/voice.xml',
-                    to='+821083782358',
-                    from_='+19204770393'
-                )
-            elif absents_new!=absents:
-                message = client.messages \
-                    .create(body='부재중 전화가 늘어난 것 같다', from_='+19204770393', to='+821083782358')
-                absents=absents_new
-            else:
-                pass
 
+            # 컴퓨터에 폰 화면이 뜨긴 하는가
             if pa.locateOnScreen('phone_vertex.png') or pa.locateOnScreen('phone_vertex2.png'):
                 #폰 모서리가 좌상, 우하단 다 보이는가? 안 보이면 오류 보고 하고, 보이면 화면 안 꺼지게 조치
                 XY=pa.locateOnScreen('phone_vertex.png')
@@ -110,6 +85,7 @@ while True and not kb.is_pressed('Esc'):
                     time.sleep(0.03)
                     pa.scroll(500)
                     if errors_XY>30:
+                        print('error_XY가 30을 넘었다:XY')
                         call = client.calls.create(
                             url='http://demo.twilio.com/docs/voice.xml',
                             to='+821083782358',
@@ -120,7 +96,12 @@ while True and not kb.is_pressed('Esc'):
                             .create(body='왼쪽 위 모서리가 가려졌다.', from_='+19204770393', to='+821083782358')
                 elif XY_ is None:
                     errors_XY += 1
+                    pa.moveTo(XY[0] + 200, XY[1] + 500)
+                    pa.scroll(-500)
+                    time.sleep(0.03)
+                    pa.scroll(500)
                     if errors_XY>30:
+                        print('error_XY가 30을 넘었다:XY_')
                         call = client.calls.create(
                             url='http://demo.twilio.com/docs/voice.xml',
                             to='+821083782358',
@@ -146,6 +127,55 @@ while True and not kb.is_pressed('Esc'):
                 else:
                     message = client.messages \
                         .create(body='모서리가 가려졌다.', from_='+19204770393', to='+821083782358')
+
+            # 폰이 꺼져있진 않는가
+            if not pa.locateOnScreen('phone_on.png',confidence=cf):
+                print("폰이 꺼져부렀으")
+                call = client.calls.create(
+                    url='http://demo.twilio.com/docs/voice.xml',
+                    to='+821035002156',
+                    from_='+19204770393'
+                )
+            # 전화기록으로 들어와 있는가
+            if not (pa.locateOnScreen('call_history.png', confidence=cf) or pa.locateOnScreen('call.png', confidence=cf)):
+                pa.click(pa.center(pa.locateOnScreen('home.png', confidence=cf)))
+                pa.sleep(5)
+                pa.click(pa.center(pa.locateOnScreen('call_history_icon.png', confidence=cf)))
+            else:
+                pass
+
+            # 최상단이긴 한가
+            if not pa.locateOnScreen('call.png', confidence=cf):
+                errors_call += 1
+                if errors_call > 5:
+                    print("errors_call 발생")
+                    call = client.calls.create(
+                        url='http://demo.twilio.com/docs/voice.xml',
+                        to='+821083782358',
+                        from_='+19204770393'
+                    )
+                else:
+                    print('전화 기록 최상단으로 이동합니다')
+                    pa.moveTo(XY_0[0] + 200, XY_0[1] + 500)
+                    for i in range(20):
+                        pa.scroll(500)
+                        pa.sleep(0.1)
+
+            elif pa.locateOnScreen('call_absent.png', confidence=cf):
+                print(pa.locateOnScreen('call_absent.png'))
+                message = client.messages \
+                    .create(body='부재중 전화가 늘어났다', from_='+19204770393', to='+821083782358')
+                call = client.calls.create(
+                    url='http://demo.twilio.com/docs/voice.xml',
+                    to='+821083782358',
+                    from_='+19204770393'
+                )
+            elif absents_new != absents:
+                message = client.messages \
+                    .create(body='부재중 전화가 늘어난 것 같다', from_='+19204770393', to='+821083782358')
+                absents = absents_new
+            else:
+                pass
             pa.moveTo(xy)
             print(f'지금 시간은.... {time.strftime("%H:%M:%S", time.localtime(time.time()))}')
             time.sleep(20)
